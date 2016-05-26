d29609719f60872fe750e629426473da5201c5fa
phananhnhat <anhnhatphan1995@gmail.com>
2016-05-26 23:45:12 +0700

    Test thu

--- BTLCongNgheWeb_Version2/Controllers/HomeController.cs
+++ BTLCongNgheWeb_Version2/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace BTLCongNgheWeb_Version2.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
+            // Dkm bon m , rat la xam l nhé
             return View();
         }
     }
